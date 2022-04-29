f57d00e0e8e89d294f4c15d5e8e50b42f382fa6d
Efrain Lemus <88905038+Efrain-L@users.noreply.github.com>
2022-04-29 09:26:29 -0700

    Add files via upload

---
+++ directories.py
@@ -0,0 +1,13 @@
+#This file contains all of the file paths and objects to hold them
+
+#ids and directories
+botChannel = 880520345348628501
+closeFile = r"C:\VSCode\Pythons\MCBOT\serverclose.exe"
+enigmaFile = r'C:\Users\efrai\Desktop\Minecraft\ServerFiles\Enigmatica6Server-0.5.9\start-server.bat'
+createFile = r'C:\Users\efrai\Desktop\Minecraft\ServerFiles\AboveandBeyond-1.3Server\ServerStart.bat'
+rlFile = r"C:\Users\efrai\Desktop\Minecraft\ServerFiles\RLCraftServer\startserver.bat"
+
+serverList = {"enigmatica" : enigmaFile, "create" : createFile, "rlcraft" : rlFile}
+
+validUsers = {"309470462352687104", "286339810396864523", "310290951161511946",
+              "310144402272026625", "528375617323008000"}
