68f301b19c57e75a89529cfd57945e2450482a72
Efrain Lemus <killdragon7257@gmail.com>
2023-01-29 17:30:10 -0800

    Configured server starting through the select menu interaction.

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -5,16 +5,14 @@ module.exports = {
 	name: Events.InteractionCreate,
 	// Slash commands are invoked through interactions
 	async execute(interaction) {
-		// Exit if the interaction is a command
+		// Handler code for slash command interactions
 	    if (interaction.isChatInputCommand()) {
 		    const command = interaction.client.commands.get(interaction.commandName);
-
 		    // Error message when the command given is not a valid command
 		    if (!command) {
 			    console.error(`No command matches ${interaction.commandName} was found.`);
 			    return;
 		    }
-
 		    // Try to execute the command, catch if an error occured
 		    try {
 			    await command.execute(interaction);
@@ -24,36 +22,45 @@ module.exports = {
 			    await interaction.reply({ content: 'There was an error trying to execute this command', ephemeral: true });
 		    }
 	    }
+        // Handler for selection menu interactions
 	    else if (interaction.isStringSelectMenu()) {
 		    // If the interaction is selecting a modpack option
 		    if (interaction.customId === 'start-select') {
 			    await interaction.update({ content: 'Attempting to start the pack...', components: [] });
 			    let modPath = '';
+                // Get the path from the selection's value
 			    await interaction.values.forEach(async value => {
 				    modPath += `${value}`;
 			    });
-			    await startCommand(modPath);
-			    let time = 0;
-			    let started = false;
-			    while (time < 240) {
-				    // console.log(`pinging (${time}s)...`);
-				    if (await ping() === 'running') {
-					    started = true;
-					    await interaction.followUp('Server has started.');
-					    break;
-				    }
-				    // sleeps for 5000 ms or 5 seconds
-				    await sleep(5000);
-				    time += 5;
-			    }
-			    if (!started) {
-				    await interaction.editReply('The server took too long to start.');
-			    }
+                // Once the path has been retrieved, call the starter method
+                await serverStarter(interaction, modPath);
 		    }
 	    }
 	},
 };
 
+// Server starter method
+async function serverStarter(interaction, modPath) {
+    await startCommand(modPath);
+    let time = 0;
+    let started = false;
+    while (time < 240) {
+        // console.log(`pinging (${time}s)...`);
+        if (await ping() === 'running') {
+            started = true;
+            await interaction.followUp('Server has started.');
+            break;
+        }
+        // sleeps for 5000 ms or 5 seconds
+        await sleep(5000);
+        time += 5;
+    }
+    if (!started) {
+        await interaction.followUp('The server took too long to start.');
+    }
+}
+
+// Executes the starting shell commands
 async function startCommand(packPath) {
 	exec(`tmux send -t 0:0 "cd ${packPath}" ENTER`);
 	exec('tmux send -t 0:0 "sh run.sh" ENTER');
