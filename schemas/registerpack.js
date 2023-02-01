323440b67df25c194127179050f621181675815b
Efrain Lemus <killdragon7257@gmail.com>
2023-02-01 13:45:00 -0800

    Removed old file paths

--- schemas/registerpack.js
+++
@@ -1,7 +0,0 @@
-const Modpack = require('./modpack');
-// This script will manually add a new modpack to the database
-Modpack.create({
-    packName: 'Vault Hunters 3',
-    filePath: '~/mcservers/vh3server',
-    ready: true
-});
